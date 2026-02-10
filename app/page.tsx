47b4c307101504bc310c8962fc2e5c2d094885da
TomasLonnblad <tomaslonnblad@gmail.com>
2026-02-10 11:24:17 +0100

    x

---
+++ app/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+
+
+import Header from "@/components/Header";
+
+import Hero from "@/components/Hero";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+export default function Home() {
+  const { scrollY } = useScroll();
+
+  const documentHeight = document.documentElement.scrollHeight;
+
+  return (
+    <div className="min-h-screen ">
+      <div className="flex justify-between">
+      
+        <div className="space-x-2 mb-2 pt-2 mr-2">
+          {/* @ts-ignore */}
+        
+        </div>
+      </div>
+
+      <motion.div
+        className="relative w-screen"
+        style={{
+          y: useTransform(scrollY, [0, 1000], [0, documentHeight], {
+            clamp: false,
+          }),
+        }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
+        <Header />
+      </motion.div>
+
+      <div className="p-2">
+        <Hero />
+      </div>
+    </div>
+  );
+}
---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+
+
+export default function Hero() {
+  return (
+    <div className="min-h-screen">
+      {/* Welcome Card */}
+
+          </div>
+  );
+}
